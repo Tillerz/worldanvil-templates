5ffc006e7b6c66b692b8d15a8fc7a4bf5737dc3b
Oliver Hertel <rom@readonly.de>
2025-07-14 19:41:05 +0200

    remove unused code

--- tools/backup/cloudflare/worker.js
+++ tools/backup/cloudflare/worker.js
@@ -9,10 +9,9 @@ addEventListener('fetch', event => {
 async function handleRequest(request) {
   const originalHostURL = 'https://www.worldanvil.com'
   const url = new URL(request.url)
-  const apiEndpoint = url.pathname.replace('/proxy', '')
   const queryString = url.search
   
-  const newUrl = `${originalHostURL}${apiEndpoint}${queryString}`
+  const newUrl = `${originalHostURL}${queryString}`
   const newRequest = new Request(newUrl, {
     method: request.method,
     headers: request.headers,
